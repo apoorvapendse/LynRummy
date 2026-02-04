e9c4fa85be80352115bd1c8063ae68eff0b784f2
Steve Howell <showell30@yahoo.com>
2026-02-04 09:03:24 -0500

    Send test message to Zulip.

--- game.ts
+++ game.ts
@@ -3411,36 +3411,6 @@ class SoundEffectsSingleton {
     }
 }
 
-function talk_to_zulip() {
-    const site = "https://macandcheese.zulipchat.com";
-
-    const my_email = "showell30@yahoo.com";
-
-    const config = {
-        username: my_email,
-        api_key: "x1D2df6fjdIjOOW6pUxuseFHHLQq7UD0",
-    };
-
-    const base_url = `${site}/api/v1/users/${my_email}`;
-    const method = "GET";
-
-    // now for the technical piece
-    const url = new URL(base_url);
-    const auth = btoa(`${config.username}:${config.api_key}`);
-    const auth_header = `Basic ${auth}`;
-    const options = { method, headers: { Authorization: auth_header } };
-
-    const promise = fetch(url.href, options);
-    promise.then((response) => {
-        const json_promise = response.json();
-        json_promise.then((data) => {
-            console.log(data);
-        });
-    });
-}
-
-talk_to_zulip();
-
 // SINGLETONS get initialized in gui().
 let SoundEffects: SoundEffectsSingleton;
 
@@ -3450,3 +3420,76 @@ function gui() {
     SoundEffects = new SoundEffectsSingleton();
     new LandingPage();
 }
+
+// ZULIP LAYER:
+
+const api_key = "EFCaswMRJKvdwbI2g5SwhPnkz0gNDQOO";
+const my_email = "showell30@yahoo.com";
+const site = "https://macandcheese.zulipchat.com";
+
+function get_headers() {
+    const auth = btoa(`${my_email}:${api_key}`);
+    const auth_header = `Basic ${auth}`;
+    return { Authorization: auth_header };
+}
+
+function form_data(params) {
+    const form_data = new FormData();
+    Object.keys(params).forEach((key) => {
+        let data = params[key];
+        if (Array.isArray(data)) {
+            data = JSON.stringify(data);
+        }
+        form_data.append(key, data);
+    });
+
+    return form_data;
+}
+
+function post_to_channel() {
+    const query = `messages`;
+
+    const params = {
+        type: "stream",
+        to: "LynRummy",
+        topic: "automated",
+        content: "test from LynRummy client",
+    };
+
+    const url = `${site}/api/v1/${query}`;
+    const method = "POST";
+
+    const options = { method, headers: get_headers(), body: form_data(params) };
+
+    const promise = fetch(url, options);
+    promise.then((response) => {
+        const json_promise = response.json();
+        json_promise.then((data) => {
+            console.log(data);
+        });
+    });
+}
+
+function get_user_info() {
+    const query = `users/${my_email}`;
+
+    const url = `${site}/api/v1/${query}`;
+    const method = "GET";
+
+    const options = { method, headers: get_headers() };
+
+    const promise = fetch(url, options);
+    promise.then((response) => {
+        const json_promise = response.json();
+        json_promise.then((data) => {
+            console.log(data);
+        });
+    });
+}
+
+function talk_to_zulip() {
+    get_user_info();
+    post_to_channel();
+}
+
+talk_to_zulip();
